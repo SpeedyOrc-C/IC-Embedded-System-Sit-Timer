bc6874eacdd6cfbaa8400ecf9fce6a99c7b3dc21
陈湛明 <chenzhanming04@foxmail.com>
2025-02-13 21:34:11 +0000

    Complete everything

--- src/routes/dashboard/logs/[id]/+page.svelte
+++ src/routes/dashboard/logs/[id]/+page.svelte
@@ -1,33 +1,8 @@
 <script lang="ts">
     import {goto} from "$app/navigation";
+    import {ReadableDuration} from "$lib";
 
     const {data} = $props()
-
-    function ReadableDuration(ms: number)
-    {
-        const s = ms / 1000
-
-        if (s < 60)
-        {
-            return `Less than 1 min`
-        }
-
-        const m = s / 60
-
-        if (m < 60)
-        {
-            return `${Math.floor(m)} min`
-        }
-
-        const h = m / 60
-
-        if (m % 60 < 1)
-        {
-            return `${Math.floor(h)} hour`
-        }
-
-        return `${Math.floor(h)} hour ${Math.floor(m % 60)} min`
-    }
 </script>
 
 <svelte:head>
--- src/routes/dashboard/stats/days/+page.svelte
+++ src/routes/dashboard/stats/days/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    import {PadZero} from "$lib";
+    import {PadZero, ReadableShortDuration} from "$lib";
     import {onMount} from "svelte";
 
     const {data} = $props()
-
-    const barScaleCoefficient = 1 / 1000 / 60 / 6
 
     let durationDays = $state(7)
     let inputStartDate = $state(`1970-01-01T00:00:00`)
@@ -68,15 +66,42 @@
         return xss
     })
 
-    onMount(()=>
+    const max1SitAtDateData = $derived.by(() =>
     {
-        const now = new Date()
+        const xs: number[] = new Array(durationDays).fill(0)
+
+        for (let day = 0; day < durationDays; day += 1)
+        {
+            const thisDayStart = new Date(startDate.getTime() + 1000 * 60 * 60 * 24 * day)
+            const thisDayEnd = new Date(thisDayStart.getTime() + 1000 * 60 * 60 * 24)
+
+            for (const [, logs] of data.devicesLogs)
+            {
+                for (const log of logs.filter(({start, end}) => thisDayStart <= start && end < thisDayEnd))
+                {
+                    xs[day] = Math.max(xs[day], log.end.getTime() - log.start.getTime())
+                }
+            }
+        }
+
+        return xs
+    })
+
+    const longestDurationMs = $derived(
+        durationData
+            .map(xs => xs.reduce((a, b) => a + b, 0))
+            .reduce((a, b) => Math.max(a, b), 0)
+    )
+
+    onMount(() =>
+    {
+        const now = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * (durationDays - 1))
         inputStartDate = `${now.getFullYear()}-${PadZero(now.getMonth() + 1)}-${PadZero(now.getDate())}T04:00:00`
     })
 
     function UpdateInputStartDateWhenDurationDaysChanges()
     {
-        const d = new Date(new Date(inputStartDate).getTime() - 1000 * 60 * 60 * 24 * durationDays)
+        const d = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * (durationDays - 1))
         inputStartDate = `${d.getFullYear()}-${PadZero(d.getMonth() + 1)}-${PadZero(d.getDate())}T04:00:00`
     }
 </script>
@@ -120,40 +145,44 @@
 
 <br>
 
-<table>
-    <thead>
-    <tr>
-        <th>Colour</th>
-        <th>Device Name</th>
-    </tr>
-    </thead>
-    <tbody>
-    {#each data.devices as {name}}
-    <tr>
-        <td></td>
-        <td>{name}</td>
-    </tr>
-    {/each}
-    </tbody>
-</table>
+<p>Legend</p>
 
-<div id="chart">
+<ul id="legend" class="my-colour-series">
+    {#each data.devicesLogs as [id,]}
+        <li>{data.devices.find(d => d.device_id == id).name}</li>
+    {/each}
+</ul>
+
+<br>
+
+<div id="chart" class="my-border">
     <div class="grid-head">Date</div>
+    <div class="grid-head">Max Sit</div>
     <div class="grid-head">Duration</div>
 
     {#each highlightedTimes as time, i}
-        <div>
+        {@const totalDurationMs = durationData[i].reduce((a, b) => a + b, 0)}
+        <div class="time">
             <code>
                 {time.getMonth() + 1}<span style="color: #bbb">/</span>{time.getDate()}
             </code>
         </div>
 
-        <div class="duration">
-            {#each durationData[i].map(x => x * barScaleCoefficient) as length}
-                <div style="width: {length}px">
-                    <!--{duration}-->
-                </div>
+        <div class="max-1-sit">
+            {#if max1SitAtDateData[i] > 0}
+                {ReadableShortDuration(max1SitAtDateData[i])}
+            {/if}
+        </div>
+
+        <div class="duration my-colour-series">
+            {#each durationData[i].map(x => x / longestDurationMs * 350) as length}
+                <div style="width: {length}px"></div>
             {/each}
+            <div style="background-color: unset; margin-left: 0.5rem">
+                {#if totalDurationMs > 0}
+                    {ReadableShortDuration(totalDurationMs)}
+                {/if}
+            </div>
         </div>
     {/each}
 </div>
@@ -167,16 +196,26 @@
         padding: 0.2rem 0.3rem;
     }
 
+    #legend {
+        & > li {
+            padding: 0.2rem 0.4rem;
+            width: fit-content;
+            color: white;
+            font-weight: bold;
+        }
+    }
+
     #chart {
         margin: 0 auto;
         display: grid;
         width: fit-content;
+        padding: 0.5rem;
 
-        grid-template-columns: auto auto;
+        grid-template-columns: auto auto auto;
     }
 
     .grid-head {
-        padding: 0.5rem 1rem;
+        padding: 0.2rem 0.5rem;
 
         background-color: white;
         position: sticky;
@@ -187,11 +226,29 @@
 
     .duration {
         display: flex;
+    }
 
-        & > div {
-            &         { background-color: cornflowerblue; }
-            &+div     { background-color: coral; }
-            &+div+div { background-color: darkolivegreen; }
+    .my-colour-series {
+        & > * {
+            & {
+                background-color: #093145;
+            }
+
+            & + * {
+                background-color: #829356;
+            }
+
+            & + * + * {
+                background-color: #BCA136;
+            }
+
+            & + * + * + * {
+                background-color: #C2571A;
+            }
         }
+    }
+
+    .time, .max-1-sit {
+        text-align: center;
     }
 </style>
