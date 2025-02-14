4121d3e27e9ca988cf83b10c3b62215855c7810c
陈湛明 <chenzhanming04@foxmail.com>
2025-02-14 12:04:37 +0000

    Remove logs and re-arrangle files

--- src/routes/dashboard/logs/[id]/+error.svelte
+++
@@ -1,5 +0,0 @@
-<svelte:head>
-    <title>Invalid device</title>
-</svelte:head>
-
-<p>You don't own this device or you're not logged in (<a href="/log-in">go to log-in page</a>).</p>
--- src/routes/dashboard/stats/+error.svelte
+++
@@ -1,5 +0,0 @@
-<svelte:head>
-    <title>Not logged in</title>
-</svelte:head>
-
-<p>Not logged in, please <a href="/log-in">log in</a>.</p>
